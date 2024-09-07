a50be818a525dcc1eb844061a3ad1fccb5eff3b0
andreock <andreock@users.noreply.github.com>
2024-09-07 14:04:52 +0000

    deploy: afb33571d2b3231e0fb4e334d03a970944d75877

--- search/all_7.js
+++ search/all_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['rect_0',['Rect',['../classRect.html',1,'Rect'],['../classRect.html#a950074d13a021e40d9b58663be3da12c',1,'Rect::Rect(GFXForms *_framework)'],['../classRect.html#ac18409eebfaecb778c57e77930b845d7',1,'Rect::Rect(GFXForms *_framework, int _width, int _heigth, int _x=0, int _y=0, int _radius=0, uint16_t _color=ST77XX_WHITE)']]]
+  ['rect_0',['Rect',['../classRect.html',1,'Rect'],['../classRect.html#a950074d13a021e40d9b58663be3da12c',1,'Rect::Rect(GFXForms *_framework)'],['../classRect.html#ac18409eebfaecb778c57e77930b845d7',1,'Rect::Rect(GFXForms *_framework, int _width, int _heigth, int _x=0, int _y=0, int _radius=0, uint16_t _color=ST77XX_WHITE)']]],
+  ['remove_1',['remove',['../classGrid.html#a22fe4b60e7391edc47fd676e63f3e92f',1,'Grid']]]
 ];
